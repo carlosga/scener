bdfd57d38723685a4092a6e4a31b3329f1a8c1f2
carlosga <carlosga05@gmail.com>
2014-01-19 18:36:37 +0100

    · BoundingFrustrum initial implementation

--- SceneR.Framework/Headers/Framework/BoundingFrustrum.hpp
+++ SceneR.Framework/Headers/Framework/BoundingFrustrum.hpp
@@ -17,17 +17,184 @@
 #ifndef BOUNDINGFRUSTRUM_HPP
 #define BOUNDINGFRUSTRUM_HPP
 
+#include <Framework/ContainmentType.hpp>
+#include <Framework/Matrix.hpp>
+#include <Framework/Plane.hpp>
+#include <Framework/PlaneIntersectionType.hpp>
+#include <Framework/Vector3.hpp>
+#include <System/Core.hpp>
+#include <vector>
+
 namespace SceneR
 {
-	namespace Framework
-	{
-		struct BoundingFrustrum
-		{
-		public:
-			BoundingFrustrum();
-			virtual ~BoundingFrustrum();
-		};
-	}
+    namespace Framework
+    {
+        struct BoundingBox;
+        struct BoundingSphere;
+        struct Ray;
+
+        /**
+         * Defines a frustum and helps determine whether forms intersect with it.
+         *
+         */
+        struct BoundingFrustrum
+        {
+        public:
+            /**
+             * Specifies the total number of corners (8) in the BoundingFrustrum.
+             */
+            static const System::Int32& CornerCount;
+
+        public:
+            /**
+             * Initializes a new instance of the BoundingFrustrum class.
+             * @param value Combined matrix that usually takes view × projection matrix.
+             */
+            BoundingFrustrum(const SceneR::Framework::Matrix& value);
+
+            /**
+             * Initializes a new instance of the BoundingFrustrum class.
+             */
+            BoundingFrustrum(const BoundingFrustrum& frustrum);
+
+            virtual ~BoundingFrustrum();
+
+        public:
+            /**
+             * Gets the bottom plane of the BoundingFrustum.
+             * @return the bottom plane of the BoundingFrustum.
+             */
+            const Plane& Bottom() const;
+
+            /**
+             * Gets the far plane of the BoundingFrustum.
+             * @return the far plane of the BoundingFrustum.
+             */
+            const Plane& Far() const;
+
+            /**
+             * Gets the left plane of the BoundingFrustum.
+             * @return the left plane of the BoundingFrustum.
+             */
+            const Plane& Left() const;
+
+            /**
+             * Gets the Matrix that describes this bounding frustum.
+             * @return the Matrix that describes this bounding frustum.
+             */
+            const SceneR::Framework::Matrix& Matrix() const;
+
+            /**
+             * Sets the Matrix that describes this bounding frustum.
+             * @param matrix the Matrix that describes this bounding frustum.
+             */
+            void Matrix(const SceneR::Framework::Matrix& matrix);
+
+            /**
+             * Gets the near plane of the BoundingFrustum.
+             * @return the near plane of the BoundingFrustum.
+             */
+            const Plane& Near() const;
+
+            /**
+             * Gets the right plane of the BoundingFrustum.
+             * @return the right plane of the BoundingFrustum.
+             */
+            const Plane& Right() const;
+
+            /**
+             * Gets the top plane of the BoundingFrustum.
+             * @return the top plane of the BoundingFrustum.
+             */
+            const Plane& Top() const;
+
+        public:
+            /**
+             * Checks whether the current BoundingFrustrum contains a BoundingBox.
+             * @param box the BoundingBox to test for overlap.
+             * @returns the extent of overlap
+             */
+            ContainmentType Contains(const BoundingBox& box) const;
+
+            /**
+             * Checks whether the current BoundingFrustrum contains another BoundingFrustrum.
+             * @param box the BoundingFrustrum to test for overlap.
+             * @returns the extent of overlap
+             */
+            ContainmentType Contains(const BoundingFrustrum& box) const;
+
+            /**
+             * Checks whether the current BoundingFrustrum contains a BoundingSphere.
+             * @param sphere the BoundingSphere to test for overlap.
+             * @returns the extent of overlap
+             */
+            ContainmentType Contains(const BoundingSphere& sphere) const;
+
+            /**
+             * Checks whether the current BoundingFrustrum contains a point.
+             * @param sphere the point to test for overlap.
+             * @returns the extent of overlap
+             */
+            ContainmentType Contains(const Vector3& point) const;
+
+            /**
+             * Gets the list of points that make up the corners of the BoundingFrustrum.
+             */
+            std::vector<Vector3> GetCorners();
+
+            /**
+             * Checks whether the current BoundingFrustrum intersects a specified BoundingBox.
+             * @param box the BoundingBox to test for intersection with.
+             * @returns true if the BoundingFrustrum and BoundingBox intersect; false otherwise.
+             */
+            bool Intersects(const BoundingBox& box) const;
+
+            /**
+             * Checks whether the current BoundingFrustrum intersects with another BoundingFrustum.
+             * @param frustrum the BoundingFrustum to test for intersection with.
+             * @returns true if both BoundingFrustrum's intersect; false otherwise.
+             */
+            bool Intersects(const BoundingFrustrum& frustrum) const;
+
+            /**
+             * Checks whether the current BoundingFrustrum intersects a specified BoundingSphere.
+             * @param sphere the BoundingSphere to test for intersection with.
+             * @returns true if the BoundingFrustrum and BoundingSphere intersect; false otherwise.
+             */
+            bool Intersects(const BoundingSphere& sphere) const;
+
+            /**
+             * Checks whether the current BoundingFrustrum intersects a specified Plane.
+             * @param sphere the BoundingFrustrum to test for intersection with.
+             * @returns the relationship between the Plane and the BoundingFrustrum.
+             */
+            PlaneIntersectionType Intersects(const Plane& plane) const;
+
+            /**
+             * Checks whether the current BoundingFrustrum intersects a specified Ray.
+             * @param sphere the BoundingSphere to test for intersection with.
+             * @returns distance at which the ray intersects the BoundingFrustrum, or 0 if there is no intersection.
+             */
+            System::Single Intersects(const Ray& ray) const;
+
+        public:
+            BoundingFrustrum& operator=(const BoundingFrustrum& frustrum);
+            bool operator==(const BoundingFrustrum& frustrum) const;
+            bool operator!=(const BoundingFrustrum& frustrum) const;
+
+        private:
+            void UpdatePlanes();
+
+        private:
+            Plane bottom;
+            Plane far;
+            Plane left;
+            Plane near;
+            Plane right;
+            Plane top;
+            SceneR::Framework::Matrix value;
+        };
+    }
 }
 
 #endif // BOUNDINGFRUSTRUM_HPP
--- SceneR.Framework/Sources/Framework/BoundingFrustum.cpp
+++ SceneR.Framework/Sources/Framework/BoundingFrustum.cpp
@@ -15,13 +15,188 @@
 //-------------------------------------------------------------------------------
 
 #include <Framework/BoundingFrustrum.hpp>
+#include <Framework/ContainmentType.hpp>
+#include <Framework/Plane.hpp>
+#include <Framework/PlaneIntersectionType.hpp>
+#include <Framework/Vector3.hpp>
+#include <stdexcept>
 
 using namespace SceneR::Framework;
 
-BoundingFrustrum::BoundingFrustrum()
-{	
+BoundingFrustrum::BoundingFrustrum(const SceneR::Framework::Matrix& value)
+    : bottom(0.0f, 0.0f, 0.0f, 0.0f)
+    , far(0.0f, 0.0f, 0.0f, 0.0f)
+    , left(0.0f, 0.0f, 0.0f, 0.0f)
+    , near(0.0f, 0.0f, 0.0f, 0.0f)
+    , right(0.0f, 0.0f, 0.0f, 0.0f)
+    , top(0.0f, 0.0f, 0.0f, 0.0f)
+    , value(value)
+{
+    this->UpdatePlanes();
+}
+
+BoundingFrustrum::BoundingFrustrum(const BoundingFrustrum& frustrum)
+    : bottom(frustrum.bottom)
+    , far(frustrum.far)
+    , left(frustrum.left)
+    , near(frustrum.near)
+    , right(frustrum.right)
+    , top(frustrum.top)
+    , value(frustrum.value)
+{
 }
 
 BoundingFrustrum::~BoundingFrustrum()
-{	
+{
+
+}
+
+const Plane& BoundingFrustrum::Bottom() const
+{
+    return this->bottom;
+}
+
+const Plane& BoundingFrustrum::Far() const
+{
+    return this->far;
+}
+
+const Plane& BoundingFrustrum::Left() const
+{
+    return this->left;
+}
+
+const SceneR::Framework::Matrix& BoundingFrustrum::Matrix() const
+{
+    return this->value;
+}
+
+void BoundingFrustrum::Matrix(const SceneR::Framework::Matrix& matrix)
+{
+    this->value = matrix;
+    this->UpdatePlanes();
+}
+
+const Plane& BoundingFrustrum::Near() const
+{
+    return this->near;
+}
+
+const Plane& BoundingFrustrum::Right() const
+{
+    return this->right;
+}
+
+const Plane& BoundingFrustrum::Top() const
+{
+    return this->top;
+}
+
+ContainmentType BoundingFrustrum::Contains(const BoundingBox& box) const
+{
+    throw std::runtime_error("Not implemented");
+}
+
+ContainmentType BoundingFrustrum::Contains(const BoundingFrustrum& frustrum) const
+{
+    throw std::runtime_error("Not implemented");
+}
+
+ContainmentType BoundingFrustrum::Contains(const BoundingSphere& sphere) const
+{
+    throw std::runtime_error("Not implemented");
+}
+
+ContainmentType BoundingFrustrum::Contains(const Vector3& point) const
+{
+    throw std::runtime_error("Not implemented");
+}
+
+std::vector<Vector3> BoundingFrustrum::GetCorners()
+{
+    throw std::runtime_error("Not implemented");
+}
+
+bool BoundingFrustrum::Intersects(const BoundingBox& box) const
+{
+    throw std::runtime_error("Not implemented");
+}
+
+bool BoundingFrustrum::Intersects(const BoundingFrustrum& frustrum) const
+{
+    throw std::runtime_error("Not implemented");
+}
+
+bool BoundingFrustrum::Intersects(const BoundingSphere& sphere) const
+{
+    throw std::runtime_error("Not implemented");
+}
+
+PlaneIntersectionType BoundingFrustrum::Intersects(const Plane& plane) const
+{
+    throw std::runtime_error("Not implemented");
+}
+
+System::Single BoundingFrustrum::Intersects(const Ray& ray) const
+{
+    throw std::runtime_error("Not implemented");
+}
+
+BoundingFrustrum& BoundingFrustrum::operator=(const BoundingFrustrum& frustrum)
+{
+    if (this != &frustrum)
+    {
+        this->value = frustrum.value;
+    }
+
+    return *this;
+}
+
+bool BoundingFrustrum::operator==(const BoundingFrustrum& frustrum) const
+{
+    return (this->value == frustrum.value);
+}
+
+bool BoundingFrustrum::operator!=(const BoundingFrustrum& frustrum) const
+{
+    return !(*this == frustrum);
+}
+
+void BoundingFrustrum::UpdatePlanes()
+{
+    // http://www.chadvernon.com/blog/resources/directx9/frustum-culling/
+
+    // Left plane
+    this->left = Plane::Normalize({ this->value.M14() + this->value.M11()
+                                  , this->value.M24() + this->value.M21()
+                                  , this->value.M34() + this->value.M31()
+                                  , this->value.M44() + this->value.M41() });
+
+    // Right plane
+    this->right = Plane::Normalize({ this->value.M14() - this->value.M11()
+                                   , this->value.M24() - this->value.M21()
+                                   , this->value.M34() - this->value.M31()
+                                   , this->value.M44() - this->value.M41() });
+
+    // Top plane
+    this->top = Plane::Normalize({ this->value.M14() - this->value.M12()
+                                 , this->value.M24() - this->value.M22()
+                                 , this->value.M34() - this->value.M32()
+                                 , this->value.M44() - this->value.M42() });
+
+    // Bottom plane
+    this->bottom = Plane::Normalize({ this->value.M14() + this->value.M12()
+                                    , this->value.M24() + this->value.M22()
+                                    , this->value.M34() + this->value.M32()
+                                    , this->value.M44() + this->value.M42() });
+
+    // Near plane
+    this->near = Plane::Normalize({ this->value.M13(), this->value.M23(), this->value.M33(), this->value.M43() });
+
+
+    // Far plane
+    this->far = Plane::Normalize({ this->value.M14() - this->value.M13()
+                                 , this->value.M24() - this->value.M23()
+                                 , this->value.M34() - this->value.M33()
+                                 , this->value.M44() - this->value.M43() });
 }
