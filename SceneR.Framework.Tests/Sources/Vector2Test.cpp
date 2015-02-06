51b3f4314d652f13145d8b632dffd39b7fbe5600
carlosga <carlosga05@gmail.com>
2015-02-06 07:57:09 +0100

    · New Vector2 unit tests ported from the Microsoft CoreFX Systeem.Numerics.Vectors test suite   https://github.com/dotnet/corefx/tree/master/src/System.Numerics.Vectors/tests

--- SceneR.Framework.Tests/Sources/Vector2Test.cpp
+++ SceneR.Framework.Tests/Sources/Vector2Test.cpp
@@ -1,6 +1,8 @@
 // Copyright (c) Carlos Guzmán Álvarez. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+#include <cmath>
+
 #include <Vector2Test.hpp>
 
 #include <Framework/Vector2.hpp>
@@ -43,4 +45,68 @@ TEST_F(Vector2Test, Lerp)
 
     EXPECT_TRUE(3.0f  == result.X());
     EXPECT_TRUE(-2.0f == result.Y());
+}
+
+// Ported from Microsoft .NET corefx System.Numerics.Vectors test suite
+TEST_F(Vector2Test, Distance)
+{
+    auto a = Vector2 { 1.0f, 2.0f };
+    auto b = Vector2 { 3.0f, 4.0f };
+
+    Single expected = (Single)std::sqrt(8);
+    Single actual   = Vector2::Distance(a, b);
+
+    EXPECT_TRUE(expected == actual);
+}
+
+// A test for Distance (Vector2f, Vector2f)
+// Distance from the same point
+// Ported from Microsoft .NET corefx System.Numerics.Vectors test suite
+TEST_F(Vector2Test, Distance2)
+{
+    auto a = Vector2 { 1.051f, 2.05f };
+    auto b = Vector2 { 1.051f, 2.05f };
+
+    Single actual = Vector2::Distance(a, b);
+
+    EXPECT_TRUE(0.0f == actual);
+}
+
+// A test for DistanceSquared (Vector2f, Vector2f)
+// Ported from Microsoft .NET corefx System.Numerics.Vectors test suite
+TEST_F(Vector2Test, DistanceSquared)
+{
+    auto a = Vector2 { 1.0f, 2.0f };
+    auto b = Vector2 { 3.0f, 4.0f };
+
+    Single expected = 8.0f;
+    Single actual   = Vector2::DistanceSquared(a, b);
+
+    EXPECT_TRUE(expected == actual);
+}
+
+// A test for Dot (Vector2f, Vector2f)
+// Ported from Microsoft .NET corefx System.Numerics.Vectors test suite
+TEST_F(Vector2Test, Dot)
+{
+    auto a = Vector2 { 1.0f, 2.0f };
+    auto b = Vector2 { 3.0f, 4.0f };
+
+    Single expected = 11.0f;
+    Single actual   = Vector2::DotProduct(a, b);
+
+    EXPECT_TRUE(expected == actual);
+}
+
+// A test for Dot (Vector2f, Vector2f)
+// Ported from Microsoft .NET corefx System.Numerics.Vectors test suite
+TEST_F(Vector2Test, DotWithPerpendicularVector)
+{
+    auto a = Vector2 { 1.55f, 1.55f };
+    auto b = Vector2 { -1.55f, 1.55f };
+
+    Single expected = 0.0f;
+    Single actual   = Vector2::DotProduct(a, b);
+
+    EXPECT_TRUE(expected == actual);
 }
