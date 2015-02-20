9d399f75cdb367ece030c9ec6db5fa098a6b7b52
carlosga <carlosga05@gmail.com>
2015-02-20 21:42:56 +0100

    · Added missing file

---
+++ SceneR.Framework/Headers/System/IDisposable.hpp
@@ -0,0 +1,20 @@
+#ifndef IDISPOSABLE_HPP
+#define IDISPOSABLE_HPP
+
+namespace System
+{
+    /**
+     * @brief Provides a mechanism for releasing unmanaged resources.
+     */
+    class IDisposable
+    {
+    public:
+        /**
+         * @brief Performs application-defined tasks associated with freeing, releasing,
+         *        or resetting unmanaged resources.
+         */
+        virtual void Dispose() = 0;
+    };
+}
+
+#endif // IDISPOSABLE_HPP
