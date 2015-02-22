61ba0d3b7071496901af6fd11d3f2b540a7c0422
carlosga <carlosga05@gmail.com>
2015-02-22 13:05:24 +0100

    · Added a simple Camera Component

---
+++ SceneR/Headers/Camera.hpp
@@ -0,0 +1,36 @@
+// Copyright (c) Carlos Guzmán Álvarez. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#ifndef CAMERA_HPP
+#define CAMERA_HPP
+
+#include <Framework/DrawableComponent.hpp>
+#include <Framework/Matrix.hpp>
+
+namespace SceneR
+{
+    namespace Sample
+    {
+        class SampleRenderer;
+
+        class Camera : public SceneR::Framework::Component
+        {
+        public:
+            Camera(SampleRenderer& renderer);
+            ~Camera() = default;
+
+        public:
+            void Initialize() override;
+            void Update(const SceneR::Framework::RenderTime& renderTime) override;
+
+        public:
+            SceneR::Framework::Matrix View;
+            SceneR::Framework::Matrix Projection;
+
+        private:
+            System::Single rotation;
+        };
+    }
+}
+
+#endif /* CAMERA_HPP */
---
+++ SceneR/Sources/Camera.cpp
@@ -0,0 +1,38 @@
+// Copyright (c) Carlos Guzmán Álvarez. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#include <Camera.hpp>
+
+#include <SampleRenderer.hpp>
+
+#include <System/Math.hpp>
+#include <Framework/Vector3.hpp>
+
+using namespace System;
+using namespace SceneR::Framework;
+using namespace SceneR::Sample;
+
+Camera::Camera(SampleRenderer& renderer)
+    : Component  { renderer }
+    , rotation   { 0 }
+    , View       { Matrix::Identity }
+    , Projection { Matrix::Identity }
+{
+}
+
+void Camera::Initialize()
+{
+    auto aspect = this->renderer.CurrentGraphicsDevice().Viewport().AspectRatio();
+
+    this->Projection = Matrix::CreatePerspectiveFieldOfView(Math::ToRadians(27), aspect, 0.5f, 10000.0f);
+}
+
+void Camera::Update(const RenderTime& renderTime)
+{
+    auto newRotation = this->rotation + renderTime.ElapsedRenderTime().TotalSeconds();
+    this->rotation   = Math::SmoothStep(this->rotation, newRotation, Math::PiOver4);
+
+    this->View = Matrix::CreateRotationY(this->rotation, { 0.0f, 0.0f, 0.0f})
+               * Matrix::CreateLookAt({ 0.0f, 0.0f, -400.0f }, Vector3::Zero, Vector3::Up);
+
+}
