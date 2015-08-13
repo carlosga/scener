e172c46b7d75ce255390a036ec48e665661a5203
carlosga <carlosga05@gmail.com>
2015-08-13 16:44:34 +0200

    · minor changes to resource generation at build time

--- SceneR.Framework/Headers/Graphics/Resources.hpp
+++ SceneR.Framework/Headers/Graphics/Resources.hpp
@@ -9,51 +9,35 @@ namespace SceneR
     {
         class Resources
         {
+        public:
+            static const std::string BasicEffect_fragString;
+            static const std::string SkinnedEffect_vertString;
+            static const std::string BasicEffect_glslString;
+            static const std::string SkinnedEffect_glslString;
+            static const std::string Structures_glslString;
+            static const std::string Common_glslString;
+            static const std::string SkinnedEffect_fragString;
+            static const std::string Lighting_glslString;
+            static const std::string BasicEffect_vertString;
         private:
             static const unsigned char BasicEffect_frag[];
             static const unsigned BasicEffect_frag_size;
-        public:
-            static const std::string BasicEffect_fragString;
-        private:
             static const unsigned char SkinnedEffect_vert[];
             static const unsigned SkinnedEffect_vert_size;
-        public:
-            static const std::string SkinnedEffect_vertString;
-        private:
             static const unsigned char BasicEffect_glsl[];
             static const unsigned BasicEffect_glsl_size;
-        public:
-            static const std::string BasicEffect_glslString;
-        private:
             static const unsigned char SkinnedEffect_glsl[];
             static const unsigned SkinnedEffect_glsl_size;
-        public:
-            static const std::string SkinnedEffect_glslString;
-        private:
             static const unsigned char Structures_glsl[];
             static const unsigned Structures_glsl_size;
-        public:
-            static const std::string Structures_glslString;
-        private:
             static const unsigned char Common_glsl[];
             static const unsigned Common_glsl_size;
-        public:
-            static const std::string Common_glslString;
-        private:
             static const unsigned char SkinnedEffect_frag[];
             static const unsigned SkinnedEffect_frag_size;
-        public:
-            static const std::string SkinnedEffect_fragString;
-        private:
             static const unsigned char Lighting_glsl[];
             static const unsigned Lighting_glsl_size;
-        public:
-            static const std::string Lighting_glslString;
-        private:
             static const unsigned char BasicEffect_vert[];
             static const unsigned BasicEffect_vert_size;
-        public:
-            static const std::string BasicEffect_vertString;
         };
     }
 }
--- SceneR.Framework/generate_resources.cmake
+++ SceneR.Framework/generate_resources.cmake
@@ -17,6 +17,20 @@ function(generate_resources_header dir output)
     file(APPEND ${output} "    {\n")
     file(APPEND ${output} "        class Resources\n")
     file(APPEND ${output} "        {\n")
+    file(APPEND ${output} "        public:\n")
+    foreach(bin ${bins})
+        # Get short filename
+        string(REGEX MATCH "([^/]+)$" filename ${bin})
+        # Replace filename spaces & extension separator for C compatibility
+        string(REGEX REPLACE "\\.| " "_" filename ${filename})
+        # Read hex data from file
+        file(READ ${bin} filedata HEX)
+        # Convert hex data for C compatibility
+        string(REGEX REPLACE "([0-9a-f][0-9a-f])" "0x\\1," filedata ${filedata})
+        # Append data to output file
+        file(APPEND ${output} "            static const std::string ${filename}String;\n")
+    endforeach()
+    file(APPEND ${output} "        private:\n")
     # Iterate through input files
     foreach(bin ${bins})
         # Get short filename
@@ -28,11 +42,8 @@ function(generate_resources_header dir output)
         # Convert hex data for C compatibility
         string(REGEX REPLACE "([0-9a-f][0-9a-f])" "0x\\1," filedata ${filedata})
         # Append data to output file
-        file(APPEND ${output} "        private:\n")
         file(APPEND ${output} "            static const unsigned char ${filename}[];\n")
         file(APPEND ${output} "            static const unsigned ${filename}_size;\n")
-        file(APPEND ${output} "        public:\n")
-        file(APPEND ${output} "            static const std::string ${filename}String;\n")
     endforeach()
     # Write footer
     file(APPEND ${output} "        };\n")
@@ -63,9 +74,21 @@ function(generate_resources_source dir output)
         # Convert hex data for C compatibility
         string(REGEX REPLACE "([0-9a-f][0-9a-f])" "0x\\1," filedata ${filedata})
         # Append data to output file
+        file(APPEND ${output} "const std::string Resources::${filename}String = std::string(Resources::${filename}, Resources::${filename} + Resources::${filename}_size);\n")
+    endforeach()
+    # Iterate through input files
+    foreach(bin ${bins})
+        # Get short filename
+        string(REGEX MATCH "([^/]+)$" filename ${bin})
+        # Replace filename spaces & extension separator for C compatibility
+        string(REGEX REPLACE "\\.| " "_" filename ${filename})
+        # Read hex data from file
+        file(READ ${bin} filedata HEX)
+        # Convert hex data for C compatibility
+        string(REGEX REPLACE "([0-9a-f][0-9a-f])" "0x\\1," filedata ${filedata})
+        # Append data to output file
         file(APPEND ${output} "const unsigned char Resources::${filename}[]   = {${filedata}};\n")
         file(APPEND ${output} "const unsigned Resources::${filename}_size     = sizeof(${filename});\n")
-        file(APPEND ${output} "const std::string Resources::${filename}String = std::string(Resources::${filename}, Resources::${filename} + Resources::${filename}_size);\n")
     endforeach()
     file(APPEND ${output} "}\n")
     file(APPEND ${output} "}\n")
