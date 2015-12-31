1c0397ad0be51d5be9f565790aaa18e0df1cc4c7
carlosga <carlos.guzman.1@outlook.es>
2015-12-31 19:57:04 +0100

    · Fixed header guard

--- Source/SceneR/Graphics/OpenGL/Platform.hpp
+++ Source/SceneR/Graphics/OpenGL/Platform.hpp
@@ -1,10 +1,10 @@
 // Copyright (c) Carlos Guzmán Álvarez. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#ifndef SCENER_SYSTEM_GRAPHICS_PLATFORM_HPP
-#define SCENER_SYSTEM_GRAPHICS_PLATFORM_HPP
+#ifndef SCENER_GRAPHICS_OPENGL_PLATFORM_HPP
+#define SCENER_GRAPHICS_OPENGL_PLATFORM_HPP
 
 #include "SceneR/Graphics/OpenGL/glad.hpp"
 #include "SceneR/Graphics/OpenGL/glad_glx.hpp"
 
-#endif // SCENER_SYSTEM_GRAPHICS_PLATFORM_HPP
+#endif // SCENER_GRAPHICS_OPENGL_PLATFORM_HPP
