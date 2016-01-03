6f82bfe45248ac3b4453af2d53875fb5afaa3a14
carlosga <carlos.guzman.1@outlook.es>
2016-01-03 13:42:31 +0100

    · constexpr usage on some math functions

--- Source/SceneR/Math/Math.hpp
+++ Source/SceneR/Math/Math.hpp
@@ -74,124 +74,199 @@ constexpr const float min_value = std::numeric_limits<float>::lowest();
  */
 constexpr const float max_value = std::numeric_limits<float>::max();
 
-/**
- * Returns a value indicating whether the specified number evaluates to negative or positive infinity.
- * @param f A single-precision floating-point number.
- * @returns true if f evaluates to negative_infinity or negative_infinity; otherwise, false.
- */
-bool is_infinity(float f) noexcept;
+constexpr bool equal(float a, float b) noexcept
+{
+    // TODO: Take a look at
+    //
+    // http://floating-point-gui.de/errors/comparison/
+    // https://randomascii.wordpress.com/2012/02/25/comparing-floating-point-numbers-2012-edition/
+    //
+    // to take care for edge cases
+
+    return (a == b);
+}
 
 /**
  * Returns a value indicating whether the specified number evaluates to positive infinity.
  * @param f A single-precision floating-point number.
  * @returns true if f evaluates to negative_infinity; otherwise, false.
  */
-bool is_negative_infinity(float f) noexcept;
+constexpr bool is_negative_infinity(float f) noexcept
+{
+    return equal(f, negative_infinity);
+}
 
 /**
  * Returns a value indicating whether the specified number evaluates to negative or positive infinity.
  * @param f A single-precision floating-point number.
  * @returns true if f evaluates to negative_infinity; otherwise, false.
  */
-bool is_positive_infinity(float f) noexcept;
+constexpr bool is_positive_infinity(float f) noexcept
+{
+    return (equal(f, positive_infinity));
+}
+
+/**
+ * Returns a value indicating whether the specified number evaluates to negative or positive infinity.
+ * @param f A single-precision floating-point number.
+ * @returns true if f evaluates to negative_infinity or negative_infinity; otherwise, false.
+ */
+constexpr bool is_infinity(float f) noexcept
+{
+    return is_positive_infinity(f) || is_negative_infinity(f);
+}
 
 /**
  * Returns a value that indicates whether the specified value is not a number (NaN).
  * @param f A single-precision floating-point number.
  * @returns true if f evaluates to not a number (NaN); otherwise, false.
  */
-bool is_nan(float f) noexcept;
+inline bool is_nan(float f) noexcept
+{
+    return std::isnan(f);
+}
 
 /**
  * Returns the absolute value of a single-precision floating-point number.
  */
-float abs(float value) noexcept;
+inline float abs(float value) noexcept
+{
+    return std::abs(value);
+}
 
 /**
  * Returns the angle whose cosine is the specified number
  */
-float acos(float value) noexcept;
+inline float acos(float value) noexcept
+{
+    return std::acos(value);
+}
 
 /**
  * Returns the angle whose sine is the specified number.
  */
-float asin(float value) noexcept;
+inline float asin(float value) noexcept
+{
+    return std::asin(value);
+}
 
 /**
  * Returns the angle whose tangent is the specified number.
  */
-float atan(float value) noexcept;
+inline float atan(float value) noexcept
+{
+    return std::atan(value);
+}
 
 /**
  * Returns the angle whose tangent is the quotient of two specified numbers.
  * @param x The x coordinate of a point.
  * @param y The y coordinate of a point.
  */
-float atan2(float x, float y) noexcept;
+inline float atan2(float x, float y) noexcept
+{
+    return std::atan2(x, y);
+}
 
 /**
  * Returns the smallest integral value that is greater than or equal to the specified double-precision floating-point number.
  */
-float ceiling(float value) noexcept;
+inline float ceiling(float value) noexcept
+{
+    return std::ceil(value);
+}
 
 /**
  * Returns the cosine of the specified angle.
  */
-float cos(float value) noexcept;
+inline float cos(float value) noexcept
+{
+    return std::cos(value);
+}
 
 /**
 * Returns the hyperbolic cosine of the specified angle.
 */
-float cosh(float value) noexcept;
+inline float cosh(float value) noexcept
+{
+    return std::cosh(value);
+}
 
 /**
  * Returns e raised to the specified power.
  */
-float exp(float d) noexcept;
+inline float exp(float d) noexcept
+{
+    return std::exp(d);
+}
 
 /**
 * Returns the largest integer less than or equal to the specified double-precision floating-point number.
 */
-float floor(float value) noexcept;
+inline float floor(float value) noexcept
+{
+    return std::floor(value);
+}
+
+/**
+ * Rounds a double-precision floating-point value to the nearest integral value.
+ */
+inline float round(float a) noexcept
+{
+    return std::round(a);
+}
+
+/**
+ * Returns the sine of the specified angle.
+ */
+inline float sin(float value) noexcept
+{
+    return std::sin(value);
+}
+
+/**
+ * Returns the hyperbolic sine of the specified angle.
+ */
+inline float sinh(float value) noexcept
+{
+    return std::sinh(value);
+}
+
+/**
+ * Returns the square root of a specified number.
+ */
+inline float sqrt(float value) noexcept
+{
+    return std::sqrt(value);
+}
+
+/**
+ * Returns the tangent of the specified angle.
+ */
+inline float tan(float value) noexcept
+{
+    return std::tan(value);
+}
+
+/**
+ * Returns the hyperbolic tangent of the specified angle.
+ */
+inline float tanh(float value) noexcept
+{
+    return std::tanh(value);
+}
 
 /**
  * Returns the remainder resulting from the division of a specified number by another specified number.
  * @param x A dividend.
  * @param y A divisor.
  */
-float ieee_remainder(float x, float y) noexcept;
-
-/**
- * Rounds a double-precision floating-point value to the nearest integral value.
- */
-float round(float a) noexcept;
-
-/**
- * Returns the sine of the specified angle.
- */
-float sin(float value) noexcept;
-
-/**
- * Returns the hyperbolic sine of the specified angle.
- */
-float sinh(float value) noexcept;
-
-/**
- * Returns the square root of a specified number.
- */
-float sqrt(float value) noexcept;
-
-/**
- * Returns the tangent of the specified angle.
- */
-float tan(float value) noexcept;
-
-/**
- * Returns the hyperbolic tangent of the specified angle.
- */
-float tanh(float value) noexcept;
-
-bool equal(float a, float b) noexcept;
+inline float ieee_remainder(float x, float y) noexcept
+{
+    // Reference: https://msdn.microsoft.com/es-es/library/system.math.ieeeremainder%28v=vs.110%29.aspx
+    // ieee_remainder = dividend - (divisor * Math.round(dividend / divisor))
+    return x - (y * round(x / y));
+}
 
 /**
  * Returns the Cartesian coordinate for one axis of a point that is defined by a given triangle and
@@ -204,7 +279,16 @@ bool equal(float a, float b) noexcept;
  * @param amount2 the normalized barycentric (areal) coordinate b3, equal to the weighting factor for vertex 3,
  *                the coordinate of which is specified in value3.
  */
-float barycentric(float value1, float value2, float value3, float amount1, float amount2) noexcept;
+constexpr float barycentric(float value1, float value2, float value3, float amount1, float amount2) noexcept
+{
+    // Reference: http://msdn.microsoft.com/en-us/library/microsoft.xna.framework.Math.barycentric(v=xnagamestudio.40).aspx
+    //  Pa = ((1 - b2 - b3) * V1a) + (b2 * V2a) + (b3 * V3a);
+    //
+    // NOTE: DirectX docs have a different formula. http://msdn.microsoft.com/en-us/library/windows/desktop/bb324330(v=vs.85).aspx
+    //  v1 + f(v2 - v1) + g(v3 - v1)
+
+    return ((1 - amount1 - amount2) * value1) + (amount1 * value2) + (amount2 * value3);
+}
 
 /**
  * Performs a Catmull-Rom interpolation using the specified positions.
@@ -214,7 +298,20 @@ float barycentric(float value1, float value2, float value3, float amount1, float
  * @param value4 the fourth position in the interpolation.
  * @param amount weighting factor.
  */
-float catmull_rom(float value1, float value2, float value3, float value4, float amount) noexcept;
+constexpr float catmull_rom(float value1, float value2, float value3, float value4, float amount) noexcept
+{
+    // Reference: http://msdn.microsoft.com/en-us/library/windows/desktop/bb324331(v=vs.85).aspx
+    // Q(s) = [(-s3 + 2s2 - s)p1 + (3s3 - 5s2 + 2)p2 + (-3s3 + 4s2 + s)p3 + (s3 - s2)p4] / 2;
+
+    float s3 = amount * amount * amount;
+    float s2 = amount * amount;
+    float s  = amount;
+
+    return ((-s3 + 2.0f * s2 - s)          * value1
+          + (3.0f * s3 - 5.0f * s2 + 2.0f) * value2
+          + (-3.0f * s3 + 4.0f * s2 + s)   * value3
+          + (s3 - s2) * value4) / 2;
+}
 
 /**
  * Restricts a value to be within a specified range.
@@ -222,7 +319,21 @@ float catmull_rom(float value1, float value2, float value3, float value4, float 
  * @param min the min value.
  * @param max the max value.
  */
-float clamp(float value, float min, float max) noexcept;
+constexpr float clamp(float value, float min, float max) noexcept
+{
+    float maxValue = ((max < min) ? min : max);
+
+    if (value < min)
+    {
+        return min;
+    }
+    else if (value > maxValue)
+    {
+        return maxValue;
+    }
+
+    return value;
+}
 
 /**
  * Calculates a hermite spline interpolation.
@@ -232,7 +343,25 @@ float clamp(float value, float min, float max) noexcept;
  * @param tangent2 source tangent 2.
  * @param amount weighting factor.
  */
-float hermite(float value1, float tangent1, float value2, float tangent2, float amount) noexcept;
+constexpr float hermite(float value1, float tangent1, float value2, float tangent2, float amount) noexcept
+{
+    // Reference: http://cubic.org/docs/hermite.htm
+    // h1(s) =  2s^3 - 3s^2 + 1
+    // h2(s) = -2s^3 + 3s^2
+    // h3(s) =   s^3 - 2s^2 + s
+    // h4(s) =   s^3 -  s^2
+    //
+    // Reference: http://msdn.microsoft.com/en-us/library/windows/desktop/bb324340(v=vs.85).aspx
+    // Q(s) = (2s3 - 3s2 + 1)v1 + (-2s3 + 3s2)v2 + (s3 - 2s2 + s)t1 + (s3 - s2)t2.
+
+    float s3 = amount * amount * amount;
+    float s2 = amount * amount;
+
+    return (2.0f  * s3 - 3.0f * s2 + 1.0f) * value1
+         + (-2.0f * s3 + 3.0f * s2)        * value2
+         + (s3    - 2.0f  * s2 + amount)   * tangent1
+         + (s3    - s2)                    * tangent2;
+}
 
 /**
  * Performs a linear interpolation between two vectors.
@@ -241,21 +370,31 @@ float hermite(float value1, float tangent1, float value2, float tangent2, float 
  * @param amount value between 0 and 1 indicating the weight of value2.
  * @returns the linear interpolation of the two vectors.
  */
-float lerp(float value1, float value2, float amount) noexcept;
+constexpr float lerp(float value1, float value2, float amount) noexcept
+{
+    // Reference: http://msdn.microsoft.com/en-us/library/bb197812.aspx
+    return (value1 + (value2 - value1) * amount);
+}
 
 /**
  * Returns the lesser of two values
  * @param value1 the first value
  * @param value2 the second value
  */
-float min(float value1, float value2) noexcept;
+constexpr float min(float value1, float value2) noexcept
+{
+    return ((value1 < value2) ? value1 : value2);
+}
 
 /**
  * Returns the greater of two values
  * @param value1 the first value
  * @param value2 the second value
  */
-float max(float value1, float value2) noexcept;
+constexpr float max(float value1, float value2) noexcept
+{
+    return ((value1 > value2) ? value1 : value2);
+}
 
 /**
  * Interpolates between two values using a cubic equation.
@@ -263,25 +402,54 @@ float max(float value1, float value2) noexcept;
  * @param value2 second vector
  * @param amount weighting value.
  */
-float smooth_step(float value1, float value2, float amount) noexcept;
+constexpr float smooth_step(float value1, float value2, float amount) noexcept
+{
+    // Reference: http://msdn.microsoft.com/en-us/library/windows/desktop/microsoft.directx_sdk.geometric.xmvectorlerp(v=vs.85).aspx
+    float t = clamp(amount, 0.0f, 1.0f);
+
+    t = t * t * (3.0f - 2.0f * t);
+
+    return lerp(value1, value2, t);
+}
 
 /**
  * Converts the given value in degrees to radians.
  * @param degrees the angle in degress.
  */
-float to_radians(float degrees) noexcept;
+constexpr float to_radians(float degrees) noexcept
+{
+    return static_cast<float>(degrees * pi / 180.0);
+}
 
 /**
  * Converts the given value in radians to degrees.
  * @param radians the angle in radians.
  */
-float to_degrees(float radians) noexcept;
+constexpr float to_degrees(float radians) noexcept
+{
+    return static_cast<float>(radians * 180.0 / pi);
+}
 
 /**
  * Reduces a given angle to a value between π and -π.
  * @param angle The angle to reduce, in radians.
  */
-float wrap_angle(float angle) noexcept;
+inline float wrap_angle(float angle) noexcept
+{
+    float result = ieee_remainder(angle, two_pi);
+
+    if (result < -pi)
+    {
+        result += two_pi;
+    }
+
+    if (result > pi)
+    {
+        result -= two_pi;
+    }
+
+    return result;
+}
 
 }}
 
