b9e0b6195117c0d53d9cdff874f616a9a40c9881
carlosga <carlos.guzman.1@outlook.es>
2016-07-07 18:03:22 +0200

    · Fix clang-format call

--- source-format.sh
+++ source-format.sh
@@ -4,4 +4,4 @@
 #
 # clang-format accepts multiple files during one run, but let's limit it to 12
 # here so we (hopefully) avoid excessive memory usage.
-find ./source \( -name \*.c -or -name \*.cpp -or -name \*.cc -or -name \*.h \) |xargs -n12 -P4 clang-format-3.6 -i
+find ./source \( -name \*.c -or -name \*.cpp -or -name \*.cc -or -name \*.h -or -name \*.hpp \) |xargs -n12 -P4 clang-format-3.8 -i %1
