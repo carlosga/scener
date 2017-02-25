dc3ae529a7c0181c1ae2167207dd2df62c8f0aa5
carlosga <carlos.guzman.1@outlook.es>
2017-02-25 10:26:07 +0100

    · C++17 namespace declaration syntax

--- source/scener/content/content_reader.cpp
+++ source/scener/content/content_reader.cpp
@@ -12,114 +12,113 @@
 #include "scener/io/file.hpp"
 #include "scener/io/path.hpp"
 
-namespace scener { namespace content {
-
-using json11::Json;
-
-content_reader::content_reader(const std::string& assetname, content::content_manager* manager, io::stream& stream) noexcept
-    : _asset_name      { assetname }
-    , _asset_reader    { stream    }
-    , _content_manager { manager   }
-    , _root            { }
+namespace scener::content
 {
-}
+    using json11::Json;
 
-const std::string& content_reader::asset_name() const noexcept
-{
-    return _asset_name;
-}
-
-content::content_manager* content_reader::content_manager() const noexcept
-{
-    return _content_manager;
-}
-
-std::shared_ptr<graphics::model> content_reader::read_asset() noexcept
-{
-    auto buffer = _asset_reader.read_bytes(_asset_reader.base_stream().length());
-    auto errors = std::string { };
-    auto model  = std::make_shared<graphics::model>();
-
-    _root = json11::Json::parse(reinterpret_cast<char*>(buffer.data()), errors);
-
-    Expects(errors.empty());
-
-    // Meshes
-    const auto& meshes = _root["meshes"].object_items();
-
-    model->_meshes.reserve(meshes.size());
-
-    std::for_each(meshes.begin(), meshes.end(), [&](const auto& mesh) -> void
+    content_reader::content_reader(const std::string& assetname, content::content_manager* manager, io::stream& stream) noexcept
+        : _asset_name      { assetname }
+        , _asset_reader    { stream    }
+        , _content_manager { manager   }
+        , _root            { }
     {
-        model->_meshes.push_back(read_object<graphics::model_mesh>(mesh.first, mesh.second));
-    });
-
-    // Nodes
-    const auto& nodes = _root["nodes"].object_items();
-
-    // Joints
-    for (const auto& node : nodes)
-    {
-        if (!node.second["jointName"].is_null())
-        {
-            read_object<gltf::node>(node.first, node.second);
-        }
-    }
-    // Other nodes
-    for (const auto& node : nodes)
-    {
-        if (node.second["jointName"].is_null())
-        {
-            read_object<gltf::node>(node.first, node.second);
-        }
     }
 
-    // Animations
-    for (const auto& animation : _root["animations"].object_items())
+    const std::string& content_reader::asset_name() const noexcept
     {
-        read_object_instance<graphics::animation>(animation.first, animation.second);
+        return _asset_name;
     }
 
-    return model;
-}
-
-bool content_reader::read_header() noexcept
-{
-    return true;
-}
-
-std::string content_reader::get_asset_path(const std::string& assetname) const noexcept
-{
-    auto root = io::path::combine(io::path::get_directory_name(_asset_name), assetname);
-
-    return io::path::combine(_content_manager->root_directory(), root);
-}
-
-std::vector<std::uint8_t> content_reader::read_external_reference(const std::string& assetname) const noexcept
-{
-    auto path = get_asset_path(assetname);
-
-    Ensures(io::file::exists(path));
-
-    return io::file::read_all_bytes(path);
-}
-
-std::shared_ptr<gltf::node> content_reader::find_joint_node(const std::string& jointname) const noexcept
-{
-    for (const auto& node : _nodes)
+    content::content_manager* content_reader::content_manager() const noexcept
     {
-        if (node.second.get())
-        {
-            auto& joint = node.second->joint;
+        return _content_manager;
+    }
 
-            if (joint.get() && joint->name() == jointname)
+    std::shared_ptr<graphics::model> content_reader::read_asset() noexcept
+    {
+        auto buffer = _asset_reader.read_bytes(_asset_reader.base_stream().length());
+        auto errors = std::string { };
+        auto model  = std::make_shared<graphics::model>();
+
+        _root = json11::Json::parse(reinterpret_cast<char*>(buffer.data()), errors);
+
+        Expects(errors.empty());
+
+        // Meshes
+        const auto& meshes = _root["meshes"].object_items();
+
+        model->_meshes.reserve(meshes.size());
+
+        std::for_each(meshes.begin(), meshes.end(), [&](const auto& mesh) -> void
+        {
+            model->_meshes.push_back(read_object<graphics::model_mesh>(mesh.first, mesh.second));
+        });
+
+        // Nodes
+        const auto& nodes = _root["nodes"].object_items();
+
+        // Joints
+        for (const auto& node : nodes)
+        {
+            if (!node.second["jointName"].is_null())
             {
-                return node.second;
+                read_object<gltf::node>(node.first, node.second);
             }
         }
+        // Other nodes
+        for (const auto& node : nodes)
+        {
+            if (node.second["jointName"].is_null())
+            {
+                read_object<gltf::node>(node.first, node.second);
+            }
+        }
+
+        // Animations
+        for (const auto& animation : _root["animations"].object_items())
+        {
+            read_object_instance<graphics::animation>(animation.first, animation.second);
+        }
+
+        return model;
     }
 
-    return nullptr;
-}
+    bool content_reader::read_header() noexcept
+    {
+        return true;
+    }
 
-}}
+    std::string content_reader::get_asset_path(const std::string& assetname) const noexcept
+    {
+        auto root = io::path::combine(io::path::get_directory_name(_asset_name), assetname);
+
+        return io::path::combine(_content_manager->root_directory(), root);
+    }
+
+    std::vector<std::uint8_t> content_reader::read_external_reference(const std::string& assetname) const noexcept
+    {
+        auto path = get_asset_path(assetname);
+
+        Ensures(io::file::exists(path));
+
+        return io::file::read_all_bytes(path);
+    }
+
+    std::shared_ptr<gltf::node> content_reader::find_joint_node(const std::string& jointname) const noexcept
+    {
+        for (const auto& node : _nodes)
+        {
+            if (node.second.get())
+            {
+                auto& joint = node.second->joint;
+
+                if (joint.get() && joint->name() == jointname)
+                {
+                    return node.second;
+                }
+            }
+        }
+
+        return nullptr;
+    }
+}
--- source/scener/graphics/index_buffer.cpp
+++ source/scener/graphics/index_buffer.cpp
@@ -6,73 +6,72 @@
 #include "scener/graphics/opengl/buffer_target.hpp"
 #include "scener/graphics/opengl/buffer_usage.hpp"
 
-namespace scener { namespace graphics {
-
-using scener::graphics::opengl::buffer;
-using scener::graphics::opengl::buffer_target;
-using scener::graphics::opengl::buffer_usage;
-
-index_buffer::index_buffer(gsl::not_null<graphics_device*> device
-                         , component_type                  index_element_type
-                         , std::size_t                     index_count) noexcept
-    : graphics_resource   { device }
-    , _buffer             { buffer_target::element_array_buffer, buffer_usage::static_draw }
-    , _index_count        { index_count }
-    , _index_element_type { index_element_type }
+namespace scener::graphics
 {
-}
+    using scener::graphics::opengl::buffer;
+    using scener::graphics::opengl::buffer_target;
+    using scener::graphics::opengl::buffer_usage;
 
-std::size_t index_buffer::index_count() const noexcept
-{
-    return _index_count;
-}
-
-component_type index_buffer::index_element_type() const noexcept
-{
-    return _index_element_type;
-}
-
-std::size_t index_buffer::element_size_in_bytes() const noexcept
-{
-    switch (_index_element_type)
+    index_buffer::index_buffer(gsl::not_null<graphics_device*> device
+                             , component_type                  index_element_type
+                             , std::size_t                     index_count) noexcept
+        : graphics_resource   { device }
+        , _buffer             { buffer_target::element_array_buffer, buffer_usage::static_draw }
+        , _index_count        { index_count }
+        , _index_element_type { index_element_type }
     {
-    case component_type::byte:
-    case component_type::ubyte:
-        return sizeof(std::uint8_t);
-    case component_type::int16:
-    case component_type::uint16:
-        return sizeof(std::uint16_t);
-    case component_type::single:
-        return sizeof(float);
+    }
+
+    std::size_t index_buffer::index_count() const noexcept
+    {
+        return _index_count;
+    }
+
+    component_type index_buffer::index_element_type() const noexcept
+    {
+        return _index_element_type;
+    }
+
+    std::size_t index_buffer::element_size_in_bytes() const noexcept
+    {
+        switch (_index_element_type)
+        {
+        case component_type::byte:
+        case component_type::ubyte:
+            return sizeof(std::uint8_t);
+        case component_type::int16:
+        case component_type::uint16:
+            return sizeof(std::uint16_t);
+        case component_type::single:
+            return sizeof(float);
+        }
+    }
+
+    std::vector<std::uint8_t> index_buffer::get_data() const noexcept
+    {
+        return get_data(0, _index_count);
+    }
+
+    std::vector<std::uint8_t> index_buffer::get_data(std::size_t start_index, std::size_t element_count) const noexcept
+    {
+        auto offset = (start_index * element_size_in_bytes());
+        auto size   = (element_count * element_size_in_bytes());
+
+        return _buffer.get_data(offset, size);
+    }
+
+    void index_buffer::set_data(const gsl::span<const std::uint8_t>& data) const noexcept
+    {
+        _buffer.set_data(_index_count * element_size_in_bytes(), data.data());
+    }
+
+    void index_buffer::bind() const noexcept
+    {
+        _buffer.bind();
+    }
+
+    void index_buffer::unbind() const noexcept
+    {
+        _buffer.unbind();
     }
 }
-
-std::vector<std::uint8_t> index_buffer::get_data() const noexcept
-{
-    return get_data(0, _index_count);
-}
-
-std::vector<std::uint8_t> index_buffer::get_data(std::size_t start_index, std::size_t element_count) const noexcept
-{
-    auto offset = (start_index * element_size_in_bytes());
-    auto size   = (element_count * element_size_in_bytes());
-
-    return _buffer.get_data(offset, size);
-}
-
-void index_buffer::set_data(const gsl::span<const std::uint8_t>& data) const noexcept
-{
-    _buffer.set_data(_index_count * element_size_in_bytes(), data.data());
-}
-
-void index_buffer::bind() const noexcept
-{
-    _buffer.bind();
-}
-
-void index_buffer::unbind() const noexcept
-{
-    _buffer.unbind();
-}
-
-}}
