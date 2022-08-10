39c88d4bf1ae5e1b427c297c51876f578eecdb4d
Carlos Guzmán Álvarez <carlos.guzman.1@outlook.es>
2022-08-10 19:16:56 +0200

    Speedup the build

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ rm -rf build
 mkdir build && cd build
 # cmake .. -DCMAKE_BUILD_TYPE=Debug
 cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang
-make -j4    
+make -j
 # strace ./build/Debug/vulkan-sample
